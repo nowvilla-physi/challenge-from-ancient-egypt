0190fb721e68fc355fe5500db2c2dad716f35d17
Tomoki-i <nowvilla.physi.311@gmail.com>
2022-03-05 00:00:00 +0900

    パッケージを修正した。

--- android/app/src/main/kotlin/com/physi/nowvilla/challenge_from_ancient_egypt/MainActivity.kt
+++ android/app/src/main/kotlin/com/physi/nowvilla/challenge_from_ancient_egypt/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.physi.nowvilla.challenge_from_ancient_egypt.challenge_from_ancient_egypt
+package com.physi.nowvilla.challenge_from_ancient_egypt
 
 import io.flutter.embedding.android.FlutterActivity
 
